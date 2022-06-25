63ae89454fbcffb7b983275179afee5d4a5795da
mahbub-sakib <mahbub.sakib418@gmail.com>
2022-06-26 01:06:08 +0600

    404 page done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+    const goToHomePage = () => {
+        const path = `/`;
+        navigate(path);
+    }
+
     return (
         <div>
-            <h2>Page 404</h2>
+            <h1 className='mt-5'>Oops!</h1>
+            <h2>404 - Page Not Found</h2>
+            <p>The page you are looking for is not valid</p>
+
+            <Button variant="primary" onClick={goToHomePage}>Go to Home Page</Button>
         </div>
     );
 };
